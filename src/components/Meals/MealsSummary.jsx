b34077c460f1f475d99218bd744501664f233893
Chimoha31 <mokochii1108@gmail.com>
2022-04-13 23:47:00 -0700

    Installed styled-components Created Meal components

---
+++ src/components/Meals/MealsSummary.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import classes from './MealsSummary.module.css';
+
+const MealsSummary = () => {
+
+  return (
+    <div className={classes.summary}>
+      <h2>Your Ordered Foods, Delivered To You</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione quam dolore id quae consequuntur quod ea quis voluptates doloremque, consectetur tenetur non eveniet.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad deleniti veniam, illo accusamus libero in eveniet exercitationem quas molestias dignissimos quam.</p>
+    </div>
+  )
+}
+
+export default MealsSummary
