1ce8d141076a82a8a7cc5cb3090d0a6b285259c1
Chimoha31 <mokochii1108@gmail.com>
2022-04-15 01:25:50 -0700

    useRef

--- src/components/Meals/MealItem/MealItemForm.jsx
+++ src/components/Meals/MealItem/MealItemForm.jsx
@@ -1,22 +1,41 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import classes from "./MealItemForm.module.css";
-import Input from '../../UI/Input';
-import { v4 as uuidv4 } from 'uuid';
+import Input from "../../UI/Input";
+import { v4 as uuidv4 } from "uuid";
 
 const MealItemForm = () => {
   const uuid = uuidv4();
+  const amountInputRef = useRef();
+  const [amountIsValid, setAmountIsValid] = useState(false);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+
+    if(enteredAmount.trim().length === 0 || enteredAmountNumber < 1 || enteredAmountNumber > 5) {
+      setAmountIsValid(false);
+      return;
+    }
+  };
 
   return (
-    <form className={classes.form}>
-      <Input label="Amount" input={{
-        id: uuid,
-        type: 'number',
-        min: "1",
-        max: "5",
-        step: "1",
-        defaultValue: "1"
-      }} />
+    <form className={classes.form} onSubmit={submitHandler}>
+      <Input
+        ref={amountInputRef}
+        label="Amount"
+        input={{
+          id: uuid,
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
       <button>+ Add</button>
+      {!amountIsValid && <p>Please enter a valid amount[1-5]</p>}
     </form>
   );
 };
